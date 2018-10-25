359cd60043bd446bd9da88865a7465ca23340cb1
David <dcxo.gg@gmail.com>
2018-10-25 21:24:30 +0200

    Using AndroidX library

--- app/src/androidTest/java/ml/dcxo/x/obwei/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ml/dcxo/x/obwei/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package ml.dcxo.x.obwei
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
--- app/src/main/java/ml/dcxo/x/obwei/UniqueActivity.kt
+++ app/src/main/java/ml/dcxo/x/obwei/UniqueActivity.kt
@@ -1,6 +1,6 @@
 package ml.dcxo.x.obwei
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class UniqueActivity : AppCompatActivity() {
